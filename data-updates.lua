beb09cebf68b4eae84f0fe33fa185d3328dafea0
Shayne Hemminger <shaynehemminger@live.com>
2025-12-28 11:01:23 -0500

    fixed superior inserter implementation

--- data-updates.lua
+++ data-updates.lua
@@ -1,5 +1,5 @@
 -- Use SE data_util
-local data_util = require"__space-exploration__.data_util"
+local data_util = require("__space-exploration__.data_util")
 
 -- SE Space Trains
 data.raw["cargo-wagon"]["space-cargo-wagon"].inventory_size = 80
@@ -9,5 +9,5 @@ data.raw["mining-drill"]["area-mining-drill"].drops_full_belt_stacks = true
 -- K2
 data.raw["mining-drill"]["kr-electric-mining-drill-mk3"].drops_full_belt_stacks = true
 -- Stackable Superior Inserters
-data_util.replace_or_add_ingredient("superior-inserter", "bulk-inserter", "stack-inserter")
-data_util.replace_or_add_ingredient("superior-long-inserter", "bulk-inserter", "stack-inserter")
+data_util.replace_or_add_ingredient("kr-superior-inserter", "bulk-inserter", "stack-inserter", 1)
+data_util.replace_or_add_ingredient("kr-superior-long-inserter", "bulk-inserter", "stack-inserter", 1)
