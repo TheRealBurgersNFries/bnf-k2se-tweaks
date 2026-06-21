8b5ddf7757a50d79e0a639f883256284df3cfd0c
Shayne Hemminger <shaynehemminger@live.com>
2026-06-21 14:39:57 -0400

    Changed Max Productivity on stages

---
+++ prototypes/recipes.lua
@@ -0,0 +1,10 @@
+-- Vanilla Silo Construction stages
+data.raw["recipes"]["rsc-construction-stage1"].maximum_productivity = 1000.0
+data.raw["recipes"]["rsc-construction-stage2"].maximum_productivity = 1000.0
+data.raw["recipes"]["rsc-construction-stage3"].maximum_productivity = 1000.0
+data.raw["recipes"]["rsc-construction-stage4"].maximum_productivity = 1000.0
+data.raw["recipes"]["rsc-construction-stage5"].maximum_productivity = 1000.0
+data.raw["recipes"]["rsc-construction-stage6"].maximum_productivity = 1000.0
+-- SE Specific Silo Construction stages
+data.raw["recipes"]["rsc-construction-stage5sesprs"].maximum_productivity = 1000.0
+data.raw["recipes"]["rsc-construction-stage6sesprs"].maximum_productivity = 1000.0
