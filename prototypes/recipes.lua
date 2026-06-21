a87ee2695c11931cc82998a3ccd5f5970b160dfd
Shayne Hemminger <shaynehemminger@live.com>
2026-06-21 19:10:59 -0400

    Fixed typo

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -6,5 +6,5 @@ data.raw["recipe"]["rsc-construction-stage4"].maximum_productivity = 1000.0
 data.raw["recipe"]["rsc-construction-stage5"].maximum_productivity = 1000.0
 data.raw["recipe"]["rsc-construction-stage6"].maximum_productivity = 1000.0
 -- SE Specific Silo Construction stages
-data.raw["recipe"]["rsc-construction-stage5sesprs"].maximum_productivity = 1000.0
-data.raw["recipe"]["rsc-construction-stage6sesprs"].maximum_productivity = 1000.0
+data.raw["recipe"]["rsc-construction-stage5-sesprs"].maximum_productivity = 1000.0
+data.raw["recipe"]["rsc-construction-stage6-sesprs"].maximum_productivity = 1000.0
